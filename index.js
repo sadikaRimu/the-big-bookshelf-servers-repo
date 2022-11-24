bdb6fee9e80ed3ea16f377b73db5747cc5464358
sadikaRimu <sadika3046@gmail.com>
2022-11-25 01:33:48 +0600

    new collection added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -20,10 +20,55 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const usersCollection = client.db('assignment12db').collection('users');
+        const booksCategoryCollection = client.db('assignment12db').collection('booksCategory');
+        const booksCollection = client.db('assignment12db').collection('books');
 
+
+        app.get('/users', async (req, res) => {
+            const query = {};
+            const user = await usersCollection.find(query).toArray();
+            res.send(user);
+        });
         app.post('/users', async (req, res) => {
             const user = req.body;
             const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email };
+            const user = await usersCollection.findOne(query);
+            res.send({ isAdmin: user?.role === 'Admin' });
+        });
+        app.put('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            // const filter = { _id: ObjectId(id) }
+            // const options = { upsert: true };
+            // const updatedDoc = {
+            //     $set: {
+            //         role: 'admin'
+            //     }
+            // }
+            // const result = await usersCollection.updateOne(filter, updatedDoc, options);
+            // res.send(result);
+            // const filter = { _id: ObjectId(id) };
+            // const options = { upsert: true };
+            // const updatedDoc = {
+            //     $set: {
+            //         role: 'Admin'
+            //     }
+            // }
+            // const result = await usersCollection.updatedOne(filter, updatedDoc, options);
+            // res.send(result);
+        });
+        app.get('/booksCategory', async (req, res) => {
+            const query = {};
+            const result = await booksCategoryCollection.find(query).project({ name: 1 }).toArray();
+            res.send(result);
+        });
+        app.post('/books', async (req, res) => {
+            const book = req.body;
+            const result = await booksCollection.insertOne(book);
             res.send(result);
         });
     }
