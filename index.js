bbba3e5ef5db04b8bae57adb206508e0b292e472
sadikaRimu <sadika3046@gmail.com>
2022-11-24 13:18:36 +0600

    user insertion added

--- index.js
+++ index.js
@@ -17,6 +17,21 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3eoxza1.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        const usersCollection = client.db('assignment12db').collection('users');
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
+    }
+    finally {
+
+    }
+}
+run().catch(console.log);
 
 app.get('/', async (req, res) => {
     res.send('the big bookshelf server is running');
