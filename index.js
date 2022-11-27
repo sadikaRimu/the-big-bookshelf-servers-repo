bb5c7d505b7b425a44f6542767a8eb8dc7dc2257
sadikaRimu <sadika3046@gmail.com>
2022-11-27 14:38:06 +0600

    payment added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -23,6 +24,7 @@ async function run() {
         const booksCategoryCollection = client.db('assignment12db').collection('booksCategory');
         const booksCollection = client.db('assignment12db').collection('books');
         const bookingCollection = client.db('assignment12db').collection('bookingItems');
+        const payCollection = client.db('assignment12db').collection('payments');
 
 
         app.get('/users', async (req, res) => {
@@ -110,9 +112,30 @@ async function run() {
             console.log(result);
             res.send(result);
         });
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await usersCollection.deleteOne(query);
+            res.send(result);
+        });
         app.get('/booksCategory', async (req, res) => {
             const query = {};
             const result = await booksCategoryCollection.find(query).project({ name: 1 }).toArray();
+            res.send(result);
+        });
+        //update book collection status
+        app.put('/books/statusAvailable/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const updateStatus = req.body;
+            const option = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    status: updateStatus.status
+                }
+            }
+            const result = await booksCollection.updateOne(filter, updatedDoc, option);
+            console.log(result);
             res.send(result);
         });
         //get method for my products
@@ -133,13 +156,12 @@ async function run() {
         });
         app.get('/books/:name', async (req, res) => {
             const name = req.params.name;
-            const query = { booksCategory: name };
+            const query = { booksCategory: name, status: 'Available' };
             const books = await booksCollection.find(query).toArray();
             const query2 = books.map(book => {
                 const bookedIem = { available: book.status !== 'Booked' }
                 return bookedIem;
             });
-            //const books2 = await booksCollection.find(query2).toArray();
             console.log(query);
             res.send(books);
         });
@@ -174,6 +196,14 @@ async function run() {
             const result = await booksCollection.updateOne(filter, updatedDoc, options);
             res.send(result);
         });
+        //get bookingItem by id for payment
+        app.get('/bookingItems/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const bookedItem = await bookingCollection.findOne(query);
+            res.send(bookedItem);
+        })
+
         app.post('/booking', async (req, res) => {
             const booking = req.body;
             const result = await bookingCollection.insertOne(booking);
@@ -199,6 +229,37 @@ async function run() {
             const result = await bookingCollection.deleteOne(query);
             res.send(result);
         });
+        //payment code
+        app.post('/create-payment-intent', async (req, res) => {
+            const booking = req.body;
+            const price = booking.price;
+            const amount = price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                currency: 'usd',
+                amount: amount,
+                "payment_method_types": [
+                    "card"
+                ]
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            })
+        });
+        //store payment info
+        app.post('/payments', async (req, res) => {
+            const payment = req.body;
+            const result = await payCollection.insertOne(payment);
+            const id = payment.bookId;
+            const filter = { _id: ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    status: 'Paid',
+                    transactionId: payment.transactionId
+                }
+            }
+            const updateResult = await bookingCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
     }
     finally {
 
